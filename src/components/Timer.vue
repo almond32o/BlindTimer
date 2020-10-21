75fa8583dd86ad47ca35b45d2c31d77a71018385
Almond <almond1e9@gmail.com>
2020-10-21 20:23:36 +0900

    Add Timer.vue

---
+++ src/components/Timer.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    {{ msg }}: {{ sec }}
+    <button @click="start">start</button>
+    <button @click="stop">stop</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+export default Vue.extend({
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      sec: 0!,
+      timerObj: 0!
+    }
+  },
+  methods: {
+    count(): void {
+      this.sec++;
+    },
+    start(): void {
+      this.timerObj = setInterval(this.count, 1000);
+      console.log(this.timerObj);
+    },
+    stop(): void {
+      clearInterval(this.timerObj);
+    }
+  }
+});
+</script>
