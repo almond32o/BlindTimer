76d322812bdfa7eaca114f95a4525b613313d404
Almond <almond1e9@gmail.com>
2020-12-11 23:35:34 +0900

    Install font files as dependencies

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,10 @@ module.exports = {
   ],
   pluginOptions: {
     electronBuilder: {
-      preload: 'src/preload.js'
+      preload: 'src/preload.js',
+      builderOptions: {
+        productName: 'BlindTimer'
+      }
     }
   }
 }
